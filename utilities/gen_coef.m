f2795c4dcf7ea404468365808765d8a764ceefb0
qy2116 <67695277+qy2116@users.noreply.github.com>
2020-07-02 00:06:23 +0800

    Update gen_coef.m

--- utilities/gen_coef.m
+++ utilities/gen_coef.m
@@ -2,7 +2,6 @@ function X = gen_coef(l,n,k)
 X = zeros(l,n);
 for ind = 1:n
     pos = randperm(l);
-%     c_pos = randperm(k);
     c_pos = k;
     temp = zeros(1,l);
 %     temp(pos(1:c_pos(1))) = spli_gaussian(1,c_pos(1),0.5);
